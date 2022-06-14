ae21ad2880e51406baea9c2884d824f6085435b3
Hayden Bradfield <hbradfield20@gmail.com>
2022-06-14 14:38:25 -0400

    create auth routes and controller

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import config from "./config/index.js";
 
+import authRoutes from "./routes/auth.routes.js";
+
 const app = express();
 app.use(express.json());
 
+app.use('/auth', authRoutes);
 
 app.listen(config.port, () => console.log(`Listening on port ${config.port}!`));
