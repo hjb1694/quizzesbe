94fd203e7060671c4493428c446f8ae2ed31aa1e
Hayden Bradfield <hbradfield20@gmail.com>
2022-06-14 14:31:03 -0400

    add to config file dotenv

--- config/index.js
+++ config/index.js
@@ -1,3 +1,10 @@
+import dotenv from "dotenv";
+import { join } from "path";
+
+dotenv.config({
+    path: join(process.cwd(), "..", ".env")
+});
+
 export default {
     env: process.env.NODE_ENV || "development",
     port: process.env.PORT || "production"
